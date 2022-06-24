94ac34cd949440fd2c6266985703ff653f93d986
Isaac Avila <isaacaldairavila@gmail.com>
2022-06-24 16:23:17 -0500

    buscador de imagenes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { Formik, Form, Field } from 'formik'
+import './header.css'
+import './content.css'
+import { useState } from 'react'
+const App = () => {
+  const [photos, setPhotos] = useState([])
+  const open = url => {
+    window.open(url)
+  }
+  console.log(photos)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div >
+      <header>
+        <Formik
+          initialValues={{ search: '' }}
+          onSubmit={
+            async values => {
+              const response = await fetch(
+                `https://api.unsplash.com/search/photos/?per_page=20&query=${values.search}`
+                , {
+                  headers: {
+                    'Authorization': 'Client-ID fZ5tPW7gjN6wdnA81BLAsZYgXMN6qOnCre7X_X50Ovo'
+                  }
+                }
+              )
+              const data = await response.json()
+              setPhotos(data.results)
+            }
+          }
         >
-          Learn React
-        </a>
+          <Form>
+            <Field name="search" />
+          </Form>
+        </Formik>
       </header>
-    </div>
+      <div className="container">
+        <div className='center'>
+          {
+            photos.map(photo =>
+              <article key={photo.id} onClick={() => open(photo.links.html)}>
+                <img src={photo.urls.regular} alt={photo.alt_description} />
+                <p>{photo.alt_description}</p>
+              </article>
+            )
+          }
+        </div>
+      </div>
+    </div >
   );
 }
 
